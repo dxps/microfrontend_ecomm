666587a8e6d8bfea286672b1d956b9d70d1d525b
dxps <marius.ileana@gmail.com>
2021-05-01 17:24:04 +0300

    feat: sharing deps

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,27 @@
 import faker from 'faker';
 
-let products = '';
 
-for (let i=0; i<3; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
+// This function gets a reference to an HTML `el`ement and
+// sets its inner content with the constructed list of products.
+const mount = (el) => {
+    let products = '';
+
+    for (let i=0; i<3; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`
+    }
+    
+    el.innerHTML = products;
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// If this code runs in isolation (development) mode, and not through the container.
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#dev-products');
+    // Ofc, this assumes that container part does not have an element with this id.
+    if (el) {
+        // Since we are running in isolation, let's mount it.
+        mount(el);
+    }
+}
+
+export { mount };
