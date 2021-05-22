c636d48e3dd87a7039515848c3521b1d544e5330
dxps <marius.ileana@gmail.com>
2021-05-22 16:31:59 +0300

    feat(sspa-d2): loading MFEs, favicon incl.

--- single-spa-demo2/readme.md
+++ single-spa-demo2/readme.md
@@ -2,6 +2,7 @@
 
 ### Run
 
+- start `sspa-d2-shell` using `cd sspa-d2-shell && yarn start`
 - start `sspa-d2-tasks-list` using `cd sspa-d2-tasks-list && && yarn server --port 8081`
 - start `sspa-d2-task-edit` using `cd sspa-d2-task-edit && yarn server --port 8082`
 
--- single-spa-demo2/sspa-d2-shell/src/sspa-d2-root-config.js
+++ single-spa-demo2/sspa-d2-shell/src/sspa-d2-root-config.js
@@ -1,13 +1,25 @@
 import { registerApplication, start } from "single-spa";
 
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
+  name: "@sspa-d2/tasks-list",
+  app: () => System.import("@sspa-d2/sspa-d2-tasks-list"),
   activeWhen: ["/"],
 });
+
+registerApplication({
+  name: "@sspa-d2/task-edit",
+  app: () => System.import("@sspa-d2/sspa-d2-task-edit"),
+  activeWhen: ["/"],
+});
+
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
 
 // registerApplication({
 //   name: "@sspa-d2/navbar",
