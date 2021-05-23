383df9ef9ccdb50c8050be3bf881ea6a3c6984b4
dxps <marius.ileana@gmail.com>
2021-05-23 16:42:47 +0300

    feat(sspa-d2): proper comps renaming.

---
+++ single-spa-demo2/sspa-d2-task-edit/src/comps/TaskEdit.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="taskedit-mfe">
+    <h2>Task Edit</h2>
+    <p>This MFE shows the task edit features.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.taskedit-mfe {
+  border: 1px solid #42b983;
+  background-color: white;
+  padding: 4px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ single-spa-demo2/sspa-d2-tasks-list/src/comps/TasksList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tasklist-mfe">
+    <h2>Tasks</h2>
+    <p>This MFE app shows the list of tasks.</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.tasklist-mfe {
+  border: 1px solid #42b983;
+  background-color: white;
+  padding: 4px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
