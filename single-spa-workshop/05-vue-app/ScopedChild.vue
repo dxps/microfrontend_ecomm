178b91159f71d938f728360eb8f34805361cc244
dxps <marius.ileana@gmail.com>
2021-06-01 23:41:12 +0300

    single-spa-workshop > 05 (css) parts & exercise

---
+++ single-spa-workshop/05-vue-app/ScopedChild.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p class="with-border">
+            The ScopedChild component uses global css that is susceptible to
+            naming collisions.
+        </p>
+        <p class="with-green-bg">
+            This paragraph, part of the ScopedChild component, has a distinct
+            and scoped style.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.with-border {
+    border: 3px solid blue;
+    font-size: 20px;
+    font-family: sans-serif;
+}
+.with-green-bg {
+    background-color: aquamarine;
+    border-radius: 6px;
+    font-family: sans-serif;
+    padding: 10px;
+}
+</style>
